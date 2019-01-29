f39d8c16e25b19be16bf93a210e60b767f086b26
nemexben <47148398+nemexben@users.noreply.github.com>
2019-01-29 21:24:52 +0200

    Add files via upload

---
+++ bc.js
@@ -0,0 +1,32 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+var prefix = "$";
+client.on("message", message => {
+
+            if (message.content.startsWith(prefix + "bc")) {
+                         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+  let args = message.content.split(" ").slice(1);
+  var argresult = args.join(' '); 
+  message.guild.members.filter(m => m.presence.status !== 'offline').forEach(m => {
+ m.send(`${argresult}\n ${m}`);
+})
+ message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'online').size}\` : عدد الاعضاء المستلمين`); 
+ message.delete(); 
+};     
+});
+
+
+client.on('ready', () => {
+   console.log(`----------------`);
+      console.log(`Desert Bot- Script By : Dark7oveRR`);
+        console.log(`----------------`);
+      console.log(`ON ${client.guilds.size} Servers '     Script By : Dark7oveRR ' `);
+    console.log(`----------------`);
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`Nemex Broadcast`,"http://twitch.tv/Dark7oveRR")
+client.user.setStatus("dnd")
+});
+
+
+
+client.login("NTM5ODg1OTkwNzA2MjgyNDk2.DzI3jw.SIn2Y_1hwFbzHkWvyGbrPdiAq3M");
