285eb454daf00f56cc5357eb3598b5102d7c96d2
nemexben <47148398+nemexben@users.noreply.github.com>
2019-01-30 18:04:40 +0200

    Update index.js

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : Dark7oveRR ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`Nemex Broadcast`,"http://twitch.tv/Dark7oveRR")
+client.user.setGame(`Moon Broadcast`,"http://twitch.tv/Dark7oveRR")
 client.user.setStatus("dnd")
 });
 
